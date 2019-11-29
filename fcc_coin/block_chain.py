ad1101e2ff4084576020abbec99d151ca2009422
shektor <s.hektor@gmail.com>
2019-11-29 11:16:31 +0000

    Create genesis block when blockchain instantiated
    A block is required to begin the chain when the BlockChain object is
    instantiated.

--- fcc_coin/block_chain.py
+++ fcc_coin/block_chain.py
@@ -3,6 +3,7 @@ class BlockChain:
     def __init__(self, block):
         self.block = block
         self.chain = []
+        self.construct_genesis()
 
     def construct_block(self, proof_no, prev_hash, data):
         block = self.block(
@@ -14,3 +15,8 @@ class BlockChain:
 
         self.chain.append(block)
         return block
+
+    def construct_genesis(self):
+        genesis_block = self.construct_block(proof_no=0, prev_hash=0, data=0)
+
+        return genesis_block
--- tests/test_block_chain.py
+++ tests/test_block_chain.py
@@ -13,9 +13,15 @@ def test_can_be_instantiated(block_chain):
     assert isinstance(block_chain, BlockChain)
 
 
-def test_can_add_new_blocks_to_chain(block_chain):
-    block_a = block_chain.construct_block(0, 0, 0)
-    block_b = block_chain.construct_block(0, 0, 0)
+def test_creates_genesis_block(block_chain):
+    genesis_block = block_chain.chain[0]
 
-    assert block_chain.chain[0] == block_a
-    assert block_chain.chain[1] == block_b
+    assert genesis_block.proof_no == 0
+    assert genesis_block.prev_hash == 0
+    assert genesis_block.data == 0
+
+
+def test_can_add_new_blocks_to_chain(block_chain):
+    block = block_chain.construct_block(0, 0, 0)
+
+    assert block_chain.chain[1] == block
